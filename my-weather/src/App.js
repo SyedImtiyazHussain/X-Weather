ebf920df4dacba507cdafd18b7321d7b38d3b4de
SyedImtiyazHussain <syedimtiyazhussain2@gmail.com>
2024-03-26 23:10:10 +0530

    resolving error

--- my-weather/src/App.js
+++ my-weather/src/App.js
@@ -47,19 +47,19 @@ export default function App() {
       {loading && <p>Loading data...</p>}
       {data && (
         <div className="container">
-          <div className="weather-card">
+          <div className="weather-cards">
             <h4>Temperature</h4>
             <p>{data.current.temp_c}°C</p>
           </div>
-          <div className="weather-card">
+          <div className="weather-cards">
             <h4>Humidity</h4>
             <p>{data.current.humidity}%</p>
           </div>
-          <div className="weather-card">
+          <div className="weather-cards">
             <h4>Condition</h4>
             <p>{data.current.condition.text}</p>
           </div>
-          <div className="weather-card">
+          <div className="weather-cards">
             <h4>Wind Speed</h4>
             <p>{data.current.wind_kph} kph</p>
           </div>
