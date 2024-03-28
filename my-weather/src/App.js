39a5c5c61dc2ab1e431ff6a38978ff91eb0647d7
SyedImtiyazHussain <syedimtiyazhussain2@gmail.com>
2024-03-28 19:42:35 +0530

    resolving error

--- my-weather/src/App.js
+++ my-weather/src/App.js
@@ -18,16 +18,17 @@ export default function App() {
       const response = await fetch(
         `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`
       );
-      if (!response.ok) {
-        throw new Error("Failed to fetch weather data");
-      }
       const result = await response.json();
+      if (result.error) {
+        alert("Failed to fetch weather data");
+      }
       setData(result);
-      setLoading(false);
     } catch (error) {
       setData(null);
       alert("Failed to fetch weather data");
       console.error("Error fetching data:", error);
+    }
+    finally {
       setLoading(false);
     }
   };
@@ -46,21 +47,21 @@ export default function App() {
 
       {loading && <p>Loading data...</p>}
       {data && (
-        <div className="container">
-          <div className="weather-cards">
-            <h4>Temperature</h4>
+        <div className="weather-cards">
+          <div className="weather-card">
+            <h3>Temperature</h3>
             <p>{data.current.temp_c}°C</p>
           </div>
-          <div className="weather-cards">
-            <h4>Humidity</h4>
+          <div className="weather-card">
+            <h3>Humidity</h3>
             <p>{data.current.humidity}%</p>
           </div>
-          <div className="weather-cards">
-            <h4>Condition</h4>
+          <div className="weather-card">
+            <h3>Condition</h3>
             <p>{data.current.condition.text}</p>
           </div>
-          <div className="weather-cards">
-            <h4>Wind Speed</h4>
+          <div className="weather-card">
+            <h3>Wind Speed</h3>
             <p>{data.current.wind_kph} kph</p>
           </div>
         </div>
